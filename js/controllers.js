0a0a1eef2ae4e61e33528d422ff91459c5cfcd08
Samir Tambe <samir.tambe1@gmail.com>
2015-09-20 14:37:48 -0400

    Created simple $http call to the msghandler php script

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,35 @@
 (function() {
-		angular.module('cardbycloud').controller('ContactCtrl', ['$scope', 'contact', function ($scope, item) {
-	    item.success(function (data) {
-	        $scope.sentback = data;
-	    });
-	    console.log("ContactCtrl");
-	}]);
+		angular.module('cardbycloud').controller('ContactCtrl', ['$scope', '$http', function ($scope, $http) {
+
+            $scope.data = {
+                flname: '',
+                email: '',
+                phonenum: '',
+                message: ''
+            };
+
+            $scope.sendReq = function() {
+                $http
+                    .post('msghandler.php', {
+                    flname: $scope.data.flname,
+                    email: $scope.data.email,
+                    phonenum: $scope.data.phonenum,
+                    message: $scope.data.message
+                })
+                .then(function (response) {
+
+                    console.log("ContactSvc Success." + response.data);
+
+                    return response;
+
+                },function (response) {
+
+                    console.log("ContactSvc Error: "+ response.data+' === '+
+                               response.status+' === '+response.statusText);
+
+                    return response;
+                });
+            };
+	   }]);
 }());
+//ContactSvc($scope.data).success(function (data) {$scope.sentback = data;});
