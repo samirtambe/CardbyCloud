973184868d52a8cb66bdc0338f246c40c9fbf411
Samir Tambe <samir.tambe1@gmail.com>
2015-09-25 00:34:11 -0400

    Removed IIFE

--- js/app.js
+++ js/app.js
@@ -1,32 +1,30 @@
-(function() {
-    angular.module('cardbycloud',['ui.router'])
-        .config( function($stateProvider, $urlRouterProvider) {
+angular.module('cardbycloud',['ui.router'])
+    .config( function($stateProvider, $urlRouterProvider) {
 
-        $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/home');
 
-        $stateProvider
-        .state('home', {
-            url: '/home',
-            templateUrl: 'views/main.html'
-        })
-        .state('about', {
-            url: '/about',
-            templateUrl: 'views/about.html'
-        })
-        .state('pricing', {
-            url: '/pricing',
-            templateUrl: 'views/pricing.html'
-        })
-        .state('contact', {
-            url: '/contact',
-            templateUrl: 'views/contact.html',
-            controller: 'ContactCtrl'
-        }).state('messagesuccess', {
-            url: '/messagesuccess',
-            templateUrl: 'views/messagesuccess.html'
-        }).state('messageerror', {
-            url: '/messageerror',
-            templateUrl: 'views/messageerror.html'
-        });
+    $stateProvider
+    .state('home', {
+        url: '/home',
+        templateUrl: 'views/main.html'
+    })
+    .state('about', {
+        url: '/about',
+        templateUrl: 'views/about.html'
+    })
+    .state('pricing', {
+        url: '/pricing',
+        templateUrl: 'views/pricing.html'
+    })
+    .state('contact', {
+        url: '/contact',
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+    }).state('messagesuccess', {
+        url: '/messagesuccess',
+        templateUrl: 'views/messagesuccess.html'
+    }).state('messageerror', {
+        url: '/messageerror',
+        templateUrl: 'views/messageerror.html'
     });
-}());
+});
